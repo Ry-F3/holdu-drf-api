7e1a888f690e838c08054d328fa33d430b6403d2
Ry-F3 <rhysfeew@gmail.com>
2024-03-10 15:30:40 +0000

    Update cloudinary setting and add env.py files

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -11,6 +11,17 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+if os.path.exists('env.py'):
+    import env
+    
+CLOUDINARY_STORAGE = {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+} 
+
+MEDIA_URL ='/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
