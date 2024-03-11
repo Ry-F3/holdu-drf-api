05ebccb043b88cd60c04fb347bae15e373451358
Ry-F3 <rhysfeew@gmail.com>
2024-03-11 14:38:06 +0000

    Added get profile types by id.

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
 from .views import EmployeeProfileView, EmployerProfileView, AdminProfileView
+from profiles import views
 
 urlpatterns = [
     path('employee-profiles/', EmployeeProfileView.as_view(),
          name='employee-profiles'),
+    path('employee-profiles/<int:pk>/', views.EmployeeProfileDetail.as_view(),
+         name='employee-profiles-detail'),
     path('employer-profiles/', EmployerProfileView.as_view(),
          name='employer-profiles'),
+    path('employer-profiles/<int:pk>/', views.EmployerProfileDetail.as_view(),
+         name='employer-profiles-detail'),
     path('admin-profiles/', AdminProfileView.as_view(), name='admin-profiles'),
+    path('admin-profiles/<int:pk>/', views.AdminProfileDetail.as_view(),
+         name='admin-profiles-detail'),
 
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
@@ -6,6 +7,9 @@ from .serializers import EmployeeProfileSerializer, EmployerProfileSerializer, A
 
 
 class EmployeeProfileView(APIView):
+    """
+    Employee Profile view.
+    """
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -15,7 +19,29 @@ class EmployeeProfileView(APIView):
         return Response(serializer.data)
 
 
+class EmployeeProfileDetail(APIView):
+    """
+    Employee Profile get by id, if id does not exist return a http 404 error.
+    """
+
+    def get_object(self, pk):
+        try:
+            employeeProfile = Profile.objects.get(
+                profile_type='employee', pk=pk)
+            return employeeProfile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        employeeProfile = self.get_object(pk)
+        serializer = EmployeeProfileSerializer(employeeProfile)
+        return Response(serializer.data)
+
+
 class EmployerProfileView(APIView):
+    """
+    Employer Profile view.
+    """
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -25,11 +51,51 @@ class EmployerProfileView(APIView):
         return Response(serializer.data)
 
 
+class EmployerProfileDetail(APIView):
+    """
+    Employer Profile get by id, if id does not exist return a http 404 error.
+    """
+
+    def get_object(self, pk):
+        try:
+            employerProfile = Profile.objects.get(
+                profile_type='employer', pk=pk)
+            return employerProfile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        employerProfile = self.get_object(pk)
+        serializer = EmployerProfileSerializer(employerProfile)
+        return Response(serializer.data)
+
+
 class AdminProfileView(APIView):
+    """
+    Admin Profile view.
+    """
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
         profiles_admin = Profile.objects.filter(profile_type='admin')
         serializer = AdminProfileSerializer(profiles_admin, many=True)
 
+        return Response(serializer.data)
+
+
+class AdminProfileDetail(APIView):
+    """
+    Admin Profile get by id, if id does not exist return a http 404 error.
+    """
+
+    def get_object(self, pk):
+        try:
+            adminProfile = Profile.objects.get(profile_type='admin', pk=pk)
+            return adminProfile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        adminProfile = self.get_object(pk)
+        serializer = AdminProfileSerializer(adminProfile)
         return Response(serializer.data)
