42ab2e9918892fffb77b278459526e97b1e77c85
Ry-F3 <rhysfeew@gmail.com>
2024-03-27 13:03:10 +0000

    Added create_profile code back.

--- profiles/models.py
+++ profiles/models.py
@@ -52,3 +52,11 @@ class Rating(models.Model):
 
     def __str__(self):
         return f"Rating: {self.rating}"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
