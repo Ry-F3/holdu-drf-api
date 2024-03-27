590a520015c1f676d2c23e6dde1c673a21893f6a
Ry-F3 <rhysfeew@gmail.com>
2024-03-27 13:15:16 +0000

    Customize create_profile to recieve profile_type.

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.utils import timezone
 
@@ -54,9 +55,11 @@ class Rating(models.Model):
         return f"Rating: {self.rating}"
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(owner=instance)
+        profile_type = instance.profile_type
+        Profile.objects.create(owner=instance, profile_type=profile_type)
 
 
 post_save.connect(create_profile, sender=User)
