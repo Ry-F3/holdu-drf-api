4cc1803cae9be16cc9398bb9a5ffa0fe6294bc6c
Ry-F3 <rhysfeew@gmail.com>
2024-03-28 12:13:12 +0000

    Remove if statement.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -125,10 +125,8 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(
-        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.gitpod\.io$",]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [r"^https://.*\.gitpod\.io$",]
 
 
 CORS_ALLOW_CREDENTIALS = True
